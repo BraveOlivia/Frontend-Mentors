fe393125d266ca48a2807270edb44cf56020a5d0
olivia <zhouxuanolivia@gmail.com>
2021-10-08 19:46:47 +1000

    add to main branch

--- 1.tip-calculator-app-main/src/components/App.jsx
+++ 1.tip-calculator-app-main/src/components/App.jsx
@@ -1,40 +1,7 @@
-import React, { useState } from "react";
-import UserInput from "./input/UserInput";
-import TipSelection from "./input/TipSelection";
-import ResultCard from "./output/ResultCard";
-import Card from "./Card";
-import "./App.css";
+import React from "react";
 
 function App() {
-  const [tip, setTip] = useState("0.00");
-  const [total, setTotal] = useState("0.00");
-  function handleTip(bill) {
-    // console.log("====================================");
-    setTip(bill);
-    // console.log("====================================");
-  }
-
-  function handleTotal(numOfPeople) {
-    setTotal(total);
-  }
-
-  return (
-    <body>
-      <header>SPLITTER</header>
-      <Card>
-        <div className="row card-content">
-          <div className="column">
-            <UserInput onAdd={handleTip} title="Bill" />
-            <TipSelection />
-            <UserInput onAdd={handleTotal} title="Number of People" />
-          </div>
-          <div className="column right-column">
-            <ResultCard tipAmount={tip} total={total} />
-          </div>
-        </div>
-      </Card>
-    </body>
-  );
+  return <h1>Test app</h1>;
 }
 
 export default App;
